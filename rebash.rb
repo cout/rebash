fa6cfa8dbc89252414f0099082147fa6e30b91b7
Paul Brannan <curlypaul924@gmail.com>
2011-05-14 14:12:07 -0400

    Colorize "ls" when it is typed.

--- rebash.rb
+++ rebash.rb
@@ -1,5 +1,14 @@
+def format_prompt(prompt)
+  return prompt
+end
+
+def format_line(line)
+  line = line.gsub(/ls/, "\033[34mls\033[m")
+  return line
+end
+
 def redisplay(prompt, line)
-  $stdout.print "\r#{prompt}: #{line}"
+  $stdout.print "\r#{format_prompt(prompt)}: #{format_line(line)}"
   $stdout.flush
 end
 
