f27c16e53a08a7e4df2d5c54371c224aa879900a
Paul Brannan <curlypaul924@gmail.com>
2011-05-20 06:58:15 -0400

    Fixed garbage at prompt.

--- lib/rebash/rebash.rb
+++ lib/rebash/rebash.rb
@@ -1,6 +1,11 @@
 require 'rebash/colorizer'
 
 def format_prompt(prompt)
+  # TODO: These characters indicate that contained between them is a
+  # non-printable sequence.  What should we do with them?
+  prompt.gsub!(/\001/, '')
+  prompt.gsub!(/\002/, '')
+
   return prompt
 end
 
@@ -10,7 +15,7 @@ def format_line(line)
 end
 
 def redisplay(prompt, line)
-  $stdout.print "\r#{format_prompt(prompt)}: #{format_line(line)}"
+  $stdout.print "\r#{format_prompt(prompt)}#{format_line(line)}"
   $stdout.flush
 end
 
